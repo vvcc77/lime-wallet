4422566f4bb99eaf06f4606b8bcba1a292d0668a
vvcc77 <112783656+vvcc77@users.noreply.github.com>
2025-10-03 17:04:40 -0300

    Update index.ts
    index parameters

--- lime-wallet-pq/node-wrapper/src/index.ts
+++ lime-wallet-pq/node-wrapper/src/index.ts
@@ -1,1 +1,10 @@
+import init, { falcon_sign } from '../../crypto-core/pkg/crypto_core.js';
+import { getQrngBytes } from './qrng.js';
+
+export async function partialSign(message: Uint8Array) {
+  await init();
+  const seed = await getQrngBytes(48);
+  // TODO: mix seed into FROST nonce
+  return falcon_sign(message);
+}
 
