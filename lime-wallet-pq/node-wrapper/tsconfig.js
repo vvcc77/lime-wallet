cc32db860c8779824853dc767bce83b0e5ef89a0
vvcc77 <112783656+vvcc77@users.noreply.github.com>
2025-10-03 17:12:42 -0300

    Update tsconfig.js
    tsconfig parameters

--- lime-wallet-pq/node-wrapper/tsconfig.js
+++ lime-wallet-pq/node-wrapper/tsconfig.js
@@ -1,1 +1,12 @@
-
+{
+  "compilerOptions": {
+    "module": "esnext",
+    "target": "es2020",
+    "rootDir": "src",
+    "outDir": "dist",
+    "moduleResolution": "node",
+    "esModuleInterop": true,
+    "sourceMap": true,
+    "strict": true
+  }
+}
